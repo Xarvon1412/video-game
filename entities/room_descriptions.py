61ce275d9733bc1c074e6e7fda173f18d61e5f28
Xarvon1412 <xarvon1412@gmail.com>
2025-05-04 09:47:10 -0600

    Initial commit for project

---
+++ entities/room_descriptions.py
@@ -0,0 +1,16 @@
+rooms = {
+    'sandy room' : {
+        'description' : 'super sandy in here',
+        'coords' : {
+            'x' : 2,
+            'y' : 2,
+        },
+    },
+    'starting room' : {
+        'description' : 'There are like...puppy posters everywhere. You gotta get outta here man.',
+        'coords' : {
+            'x' : 2,
+            'y' : 3,
+        },
+    },
+}
