af20a4f2b91ead7719d10adf181e98e908f6a555
Xarvon1412 <xarvon1412@gmail.com>
2025-05-06 16:21:10 -0600

    feat: add entity manager

--- entities/main.py
+++ entities/main.py
@@ -1,6 +1,5 @@
-#import room_descriptions
 from room_descriptions import rooms
-#rooms = room_descriptions.rooms
+from uuid import uuid4
 game_on = True
 commands = ["quit"]
 
@@ -17,6 +16,16 @@ class Player:
     def __init__(self):
         self.position = Position(x=2, y=3)
         self.health = Health(100)
+
+class EntityManager:
+    def __init__(self):
+        self.entities = set()
+
+    def create_entity(self, *components):
+        new_entity = {}
+        entity_id = uuid4()
+        new_entity.update({entity_id: list(components)})
+
 
 class MovementSystem:
     direction_map = {
@@ -44,21 +53,24 @@ class InputSystem:
         else:
             game_on = False
 
-
 class RenderSystem:
     @staticmethod
     def render(player_position):
-#        for room in rooms.values():
-#            print(room["description"])
         for room in rooms.values():
             if player_position.x == room['coords']['x'] and player_position.y == room['coords']['y']:
                 print(room['description'])
+                return
+        print("Huh. You shouldn't be here. Don't know how you got out of bounds, but you should probably go back")
 
 
 new_player = Player()
+entity_manager = EntityManager()
 
 while game_on:
-    print((new_player.position.x, new_player.position.y))
+    entity_manager.create_entity('health', 'left arm', 'head')
+    id = uuid4()
+    print(id)
+    #    print((new_player.position.x, new_player.position.y))
     RenderSystem.render(new_player.position)
     user_input = InputSystem.check_input_type()
     MovementSystem.move(new_player, user_input)
--- entities/room_descriptions.py
+++ entities/room_descriptions.py
@@ -1,16 +1,51 @@
 rooms = {
-    'sandy room' : {
+    'Sandy Room' : {
         'description' : 'super sandy in here',
         'coords' : {
             'x' : 2,
             'y' : 2,
         },
     },
-    'starting room' : {
+    'Starting Room' : {
         'description' : 'There are like...puppy posters everywhere. You gotta get outta here man.',
         'coords' : {
             'x' : 2,
             'y' : 3,
+        },
+    },
+    'Snowy Room' : {
+        'description' : "It's so freaking cold in here. Are those...are your toes blues?? Go man go!",
+        'coords' : {
+            'x' : 1,
+            'y' : 2,
+        },
+    },
+    'Lava Room' : {
+        'description' : "It's super bright and hot in here. It's like there's lava everywhere. Well, there is, but it's like there is too.",
+        'coords' : {
+            'x' : 1,
+            'y' : 1,
+        },
+    },
+    'Treasure Room' : {
+        'description' : "Congrats! You found the treasure! It was gold. Yes really. What else were you expecting? Greedy little shit.",
+        'coords' : {
+            'x' : 2,
+            'y' : 1,
+        },
+    },
+    'Dark Room' : {
+        'description' : "You can't see shit in here. It's dark. Only way to go from here is...actually you don't know. Good luck.",
+        'coords' : {
+            'x' : 3,
+            'y' : 1,
+        },
+    },
+    'Wet Room' : {
+        'description' : "Eeewww. You're pretty sure you stepped on a frog coming in here. Also it smells like mire and mildew. They should really check the crown in here to make sure it's not molding.",
+        'coords' : {
+            'x' : 3,
+            'y' : 2,
         },
     },
 }
